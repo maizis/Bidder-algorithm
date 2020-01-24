6a3c292e3f32043dd65fa01319bcfc8293ea7f43
Sarah Maizi <sarahmaizi@MacBook-Pro-de-Sarah.local>
2020-01-24 12:04:22 +0100

    add function

---
+++ data/items.js
@@ -0,0 +1,53 @@
+const item = [
+  {
+    name: 'Item 1',
+    price: 100,
+    bids: [
+      {
+        name: 'Alice',
+        bid: 200
+      },
+      {
+        name: 'Jean',
+        bid: 901
+      },
+      {
+        name: 'Clément',
+        bid: 800
+      },
+      {
+        name: 'Alice',
+        bid: 100
+      }
+    ]
+  },
+  // {
+  //   name: 'Item 2',
+  //   price: 200,
+  //   bids: []
+  // },
+  // {
+  //   name: 'Item 3',
+  //   price: 300,
+  //   bids: [
+  //     {
+  //       name: 'Alice',
+  //       bid: 280
+  //     },
+  //     {
+  //       name: 'Jean',
+  //       bid: 305
+  //     },
+  //     {
+  //       name: 'Clément',
+  //       bid: 350
+  //     },
+  //     {
+  //       name: 'Alice',
+  //       bid: 330
+  //     }
+  //   ]
+  // }
+]
+
+module.exports = items;
---
+++ sum.js
@@ -0,0 +1,37 @@
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
+// `
+// Notre object vos 100€
+
+// Alice met 99 €
+
+// Pierre met 105 €
+
+// Jean met 80 €
+
+// Clément met 70 €
+
+// ==
+
+// Pierre gagne et paye 102 €
+
+// ==
+
+
+// `
+
+// `
+// Object: 100€
+
+// Acheteur 1 : 110€ / 130€
+// Acheteur 2 : -
+// Acheteur 3 : 125€
+// Acheteur 4 : 105€ / 115 € / 90 €
+// Acheteur 5 : 132 € / 135€ / 140 €
+
+// Acheteur 5 qui gagne et paye 130 €
+// `
